a18959e48aa3ce6794a13f97e798f9a5d6317a2d
gindibay <gurkanindibay@gmail.com>
2021-02-23 11:33:40 +0300

    remove tests fr testing

--- build-execute.sh
+++ build-execute.sh
@@ -6,4 +6,4 @@ citus_indent --check
 black --check .
 /app/pg_auto_failover/ci/banned.h.sh
 make -j5 CFLAGS=-Werror
-PATH=`pg_config --bindir`:$PATH make test
+#PATH=`pg_config --bindir`:$PATH make test
